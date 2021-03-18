c2212e2320cd4fbd81e226f2d4bcb4997ded5d42
Leandro <leandro.m.sobrino@gmail.com>
2021-03-18 11:31:13 -0300

    Ejercicio 14 terminado

---
+++ Ejercicio 14/ClassLibrary1/CalculoDeArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class CalculoDeArea
+    {
+        /// <summary>
+        /// Calcular el area de un cuadrado.
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        public static double CalcularCuadrado(double valor)
+        {
+            return Math.Pow(valor, 2);            
+        }
+        /// <summary>
+        /// Calcular el area de un triangulo.
+        /// </summary>
+        /// <param name="basee"></param>
+        /// <param name="altura"></param>
+        /// <returns></returns>
+        public static double CalcularTriangulo(double basee ,double altura)
+        {
+            return (basee * altura)/(double)2;
+        }
+        /// <summary>
+        /// Calcular el area de un circulo.
+        /// </summary>
+        /// <param name="radio"></param>
+        /// <returns></returns>
+        public static double CalcularCirculo(double radio)
+        {
+            return Math.Round(Math.PI*Math.Pow(radio,2),2);
+        }
+    }
+}
---
+++ Ejercicio 14/Ejercicio 14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Ejercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            double ladoCuadrado;
+            double areaCuadrado;
+            Console.Write("Ingrese la medida de uno de los lados del cuadrado: ");
+            ladoCuadrado = double.Parse(Console.ReadLine());
+            areaCuadrado = CalculoDeArea.CalcularCuadrado(ladoCuadrado);
+            Console.Write("El area del cuadrado es de: {0} Cm2",areaCuadrado);
+            */
+            /*
+            double baseTriangulo;
+            double alturaTriangulo;
+            double areaTriangulo;
+            Console.Write("Ingrese la base del triangulo: ");
+            baseTriangulo = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la altura del triangulo: ");
+            alturaTriangulo = double.Parse(Console.ReadLine());
+            areaTriangulo = CalculoDeArea.CalcularTriangulo(baseTriangulo,alturaTriangulo);
+            Console.Write("El area del triangulo es de: {0} Cm2", areaTriangulo);
+            */
+            double radio;
+            double areaCirculo;
+            Console.Write("Ingrese el radio del circulo: ");
+            radio = double.Parse(Console.ReadLine());
+            areaCirculo = CalculoDeArea.CalcularCirculo(radio);
+            Console.Write("El area del ciruclo es de: {0} Cm2", areaCirculo);
+            Console.ReadKey();
+        }
+    }
+}
