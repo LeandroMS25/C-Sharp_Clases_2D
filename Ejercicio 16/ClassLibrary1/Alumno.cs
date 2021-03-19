0c4d2dae908c0dca4c89c43a4a78917ef8df45ec
Leandro <leandro.m.sobrino@gmail.com>
2021-03-19 17:11:35 -0300

    Ejercicio 16 casi terminado

---
+++ Ejercicio 16/ClassLibrary1/Alumno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string name;
+        public string lastName;
+        public int legajo;
+        public Alumno(string name,string lastName, int legajo) 
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.legajo = legajo;
+        }
+        public string Mostrar()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Nombre: {this.name}");
+            stringBuilder.Append($"Apellido: {this.lastName}");
+            stringBuilder.Append($"Legajo: {this.legajo}");
+            if (this.notaFinal != -1)
+            {
+                stringBuilder.Append($"Nota Final: {this.notaFinal}");
+            }
+            else
+            {
+                stringBuilder.Append("Alumno desaprobado.");
+            }
+            return stringBuilder.ToString();
+        }
+        public void Estudiar(byte notaUno, byte notaDos) 
+        {
+            this.nota1 = notaUno;
+            this.nota2 = notaDos;
+        }
+        public void CalcularFinal()
+        {
+            if (this.nota1 >= 4 && this.nota2 >= 4)
+            {
+                /*
+                Random rdm = new Random();
+                this.notaFinal = rdm.Next(4,10);
+                */
+                this.notaFinal = (float)(this.nota1 + this.nota2) / (float)2;
+            }
+            else
+            {
+                this.notaFinal = -1;
+            }
+        }
+    }
+}
---
+++ Ejercicio 16/Ejercicio 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+   
+namespace Ejercicio_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alum1 = new Alumno("Leandro","Sobrino",1094959);
+            alum1.Estudiar(10,8);
+            alum1.CalcularFinal();
+            Console.WriteLine(alum1.Mostrar());
+            Console.ReadKey();
+        }
+    }
+}
