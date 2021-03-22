dd34cd53be644ffb050376337a9363d8cf27f064
Leandro <leandro.m.sobrino@gmail.com>
2021-03-22 11:12:39 -0300

    Ejercicio 17 iniciando

---
+++ Ejercicio 17/ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+        public Boligrafo(short tinta, ConsoleColor color) 
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+        public ConsoleColor GetColor() 
+        {
+            return color;
+        }
+        public short GetTinta()
+        {
+            return tinta;
+        }
+        private void SetTinta(short tinta) 
+        {
+            this.tinta = tinta;
+        }
+        public void Recargar() 
+        {
+            this.tinta = cantidadTintaMaxima;
+        }
+    }
+}
---
+++ Ejercicio 17/Ejercicio 17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Ejercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boligrafo Rojo = new Boligrafo(50,ConsoleColor.Red);
+            Boligrafo Azul = new Boligrafo(100,ConsoleColor.Blue);
+            Console.ReadKey();
+        }
+    }
+}
