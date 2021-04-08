b84c2b55bcbb2a5bd9b7732f104adfec93fdd873
LeandroMS25 <leandro.m.sobrino@gmail.com>
2021-04-08 10:24:43 -0300

    Colecciones y propiedades

---
+++ Ejercicio 33/Bibliotica/Libro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotica
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+
+        public string this[int i] 
+        {
+            get 
+            {
+                string pagina = "Vacio";
+                if (i >= 0 && i < paginas.Count)
+                {
+                    return this.paginas[i];
+                }
+                else 
+                {
+                    return pagina;
+                }
+            }
+            set 
+            {
+                if (i >= 0 && i < paginas.Count)
+                {
+                    this.paginas[i] = value;
+                }
+                else
+                {
+                    this.paginas.Add(value);
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio 33/Ejercicio 33/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bibliotica;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+            for (int i = 0; i < 5; i++)
+            {
+                libro[i] = String.Format("{0:0#}", i);
+            }
+
+            libro[6] = "La puta madre";
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Indice: {i} Pagina: {libro[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
